e7996516655e3e71679f753b9098099da7280277
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2018-08-28 23:07:36 -0600

    clean up previous searches

--- app/interactions/searches_controller/previous_searches.rb
+++ app/interactions/searches_controller/previous_searches.rb
@@ -9,16 +9,23 @@ class SearchesController::PreviousSearches
   end
 
   def call
-    order     = params[:sort_by].present? ? params[:sort_by].to_sym : :query
-    direction = params[:sort_direction].present? ? params[:sort_direction].to_sym : :asc
-      
-    case order
+    set_sorters
+    return_query
+  end
+  
+  def return_query
+    case @order
     when :query
-      Search.limit(50).order(query: direction)
+      Search.limit(50).order(query: @direction)
     when :updated_at
-      Search.limit(50).order(updated_at: direction)
+      Search.limit(50).order(updated_at: @direction)
     when :count
-      Search.limit(50).select('searches.*, array_length(previous,1) as count').group('searches.id, count').order("count #{direction.to_s}")
+      Search.limit(50).select('searches.*, array_length(previous,1) as count').group('searches.id, count').order("count #{@direction.to_s}")
     end
+  end
+
+  def set_sorters
+    @order     = params[:sort_by].present? ? params[:sort_by].to_sym : :query
+    @direction = params[:sort_direction].present? ? params[:sort_direction].to_sym : :asc
   end
 end
